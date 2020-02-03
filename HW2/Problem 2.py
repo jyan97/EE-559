fb25c351a36af9c6a54950b9e2b8384c449e2f81
jyan97 <ethanyan@163.com>
2020-02-03 13:17:08 -0800

    HW2

--- HW2/Problem 2.py
+++ HW2/Problem 2.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 from plotDecBoundaries import plotDecBoundaries  # datasets and script are supposed to be in same directory
+from scipy.spatial.distance import cdist
 
 train_data_dir = 'wine_train.csv'  # datasets and script are supposed to be in same directory
 test_data_dir = 'wine_test.csv'
@@ -29,9 +30,23 @@ def read_and_mean(directory):
         complement_y = [y_data[m] for m in range(len(label)) if label[m] != i + 1]
         data_x = [x_data[m] for m in range(len(label)) if label[m] == i + 1]
         data_y = [y_data[m] for m in range(len(label)) if label[m] == i + 1]
-        class_mean.append([sum(data_x)/len(data_x), sum(data_y)/len(data_y),
+        class_mean.append([sum(data_x) / len(data_x), sum(data_y) / len(data_y),
                            sum(complement_x) / len(complement_x), sum(complement_y) / len(complement_y)])
-    return class_mean
+    return list(map(list, zip(*[x_data, y_data]))), label, class_mean
 
 
-print(read_and_mean('wine_train.csv'))
+# def cal_bound(mean_arr):
+#     return [[(m[0]-m[2])/(m[3]-m[1]), (m[2]**2-m[0]**2+m[3]**2-m[1]**2)/(2*(m[3]-m[1]))] for m in mean_arr]
+
+def judge_and_err(input_data, mean):
+    ss = [1 if (m[0] - i[0]) ** 2 + (m[1] - i[1]) ** 2 < (m[2] - i[0]) ** 2 + (m[3] - i[1]) ** 2 else 0 for i in
+          input_data for m in mean]  # direct distance calculation instead of calling cdist function
+    judged = list(zip(*[iter(ss)] * 3))  # using iteration to group every 3 of the elements in the list
+    # judged = [(1, 0, 0), (1, 0, 0), (1, 1, 0)...]
+    seq = [m.index(1) + 1 if m.count(1) == 1 else 0 for m in judged]
+    # seq = [1, 1, 0, 1, 1, 1, ..., 2, 0 ...]
+    return judged, seq
+
+
+train_data, train_label, train_mean = read_and_mean('wine_train.csv')
+judged, seq = judge_and_err(train_data, train_mean)
--- HW2/test.py
+++ HW2/test.py
@@ -1,20 +1,7 @@
-with open('wine_train.csv', 'r') as train_data:
-    reader = train_data.read().splitlines()
-    x_data, y_data, label = [], [], []
-    for rows in reader:
-        rows = rows.split(',')
-        x_data.append(rows[0])
-        y_data.append(rows[1])
-        label.append(rows[13])
-    x_data = list(map(float, x_data))
-    y_data = list(map(float, y_data))
-    label = list(map(int, label))
+from scipy.spatial.distance import cdist
 
-    data_x = [x_data[m] for m in range(len(label)) if label[m] == 1]
-    data_y = [m for m in y_data if label[y_data.index(m)] == 1]
 
-    complement_x = [m for m in x_data if label[x_data.index(m)] != 1]
-    complement_y = [m for m in y_data if label[y_data.index(m)] != 1]
-
-# print(sum(data_x)/len(data_x))
-print(data_x)
+a = [1,2]
+b = [2,3]
+c = cdist(a,b,metric='euclidean')
+print(c)
